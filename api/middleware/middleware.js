4bf6b0a0f7f972469910008fc17a831d4b430cbc
Emre <ozturkemre@mail.com>
2023-06-21 18:16:37 +0300

    s13g3 tamamlandı

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,61 @@
+const userModel = require("../users/users-model")
+
 function logger(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  let reqMethod = req.method
+  let reqUrl = req.originalUrl
+  let timestamp = new Date().toLocaleString();
+  console.log(`${timestamp} - ${reqMethod} - ${reqUrl}`)
+  next()
 }
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  try {
+    const existUser = await userModel.getById(req.params.id)
+    if(!existUser){
+      res.status(404).json({message:"user not found"})
+    } else {
+      req.currentUser = existUser
+      next();
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
 function validateUser(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  try {
+    let{name} = req.body;
+    if(!name){
+      res.status(400).json({message:"gerekli name alanı eksik"})
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
 function validatePost(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  try {
+    let text = req.body.text;
+    if(!text){
+      res.status(400).json({message:"gerekli text alanı eksik"})
+    }else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
 }
 
 // bu işlevleri diğer modüllere değdirmeyi unutmayın
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+  logger
+}
--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,44 +1,89 @@
 const express = require('express');
+const userModel = require("./users-model")
+const postsModel = require("../posts/posts-model")
+const mw = require("../middleware/middleware")
 
 // `users-model.js` ve `posts-model.js` sayfalarına ihtiyacınız var
 // ara yazılım fonksiyonları da gereklidir
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res, next) => {
   // TÜM KULLANICILARI İÇEREN DİZİYİ DÖNDÜRÜN
+  try {
+    let allUsers = await userModel.get();
+    res.json(allUsers)
+  } catch (error) {
+    res.status(500).json({message:"Hata oluştu"})
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', mw.validateUserId, (req, res, next) => {
   // USER NESNESİNİ DÖNDÜRÜN
   // user id yi getirmek için bir ara yazılım gereklidir
+  try {
+    res.json(req.currentUser)
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', mw.validateUser, async (req, res, next) => {
   // YENİ OLUŞTURULAN USER NESNESİNİ DÖNDÜRÜN
   // istek gövdesini doğrulamak için ara yazılım gereklidir.
+  try {
+    const insertedUser = await userModel.insert({name:req.body.name})
+    res.status(201).json(insertedUser)
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',mw.validateUserId,mw.validateUser, async (req, res, next) => {
   // YENİ GÜNCELLENEN USER NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan ara yazılım gereklidir
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
+  try {
+    const updatedUser = await userModel.update(req.params.id,{name:req.body.name})
+    res.json(updatedUser)
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',mw.validateUserId, async (req, res, next) => {
   // SON SİLİNEN USER NESNESİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
+  try {
+    await userModel.remove(req.params.id)
+    res.json(req.currentUser)
+  } catch (error) {
+    next(error)
+  }
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts',mw.validateUserId, async (req, res, next) => {
   // USER POSTLARINI İÇEREN BİR DİZİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
+  try {
+    const userPosts = await userModel.getUserPosts(req.params.id);
+    res.json(userPosts);
+  } catch (error) {
+   next(error) 
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
-  // YENİ OLUŞTURULAN KULLANICI NESNESİNİ DÖNDÜRÜN
+router.post('/:id/posts',mw.validateUserId, mw.validatePost, async (req, res) => {
+  // YENİ OLUŞTURULAN Post NESNESİNİ DÖNDÜRÜN
   // user id yi doğrulayan bir ara yazılım gereklidir.
   // ve istek gövdesini doğrulayan bir ara yazılım gereklidir.
+  try {
+    let insertedPost = await postsModel.insert({user_id:req.params.id,text:req.body.text})
+    res.status(201).json(insertedPost)
+  } catch (error) {
+    next(error)
+  }
 });
 
 // routerı dışa aktarmayı unutmayın
+module.exports = router;
